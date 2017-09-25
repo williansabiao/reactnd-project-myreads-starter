e4a96d60a41217ed89f63ef691ee90c3c18d6adb
Willian R. Sabião <willianjedai@gmail.com>
2017-09-26 00:22:31 +0100

    feat: select the correct value in shelf selector

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -8,13 +8,13 @@ class Book extends Component {
 
   render () {
     const { book } = this.props;
-    
+
     return (
       <div className="book">
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.smallThumbnail}")` }}></div>
           <div className="book-shelf-changer">
-            <select>
+            <select value={book.shelf}>
               <option value="none" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
