7afc54c60492413b1fdb5cc759811c69ac79b69a
Willian R. Sabião <willianjedai@gmail.com>
2017-10-25 13:26:21 +0100

    refactor: BookShelf component to Stateless Functional Component

--- src/components/BookShelf/index.js
+++ src/components/BookShelf/index.js
@@ -1,40 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Book from '../Book';
 
-class BookShelf extends Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    books: PropTypes.array,
-    shelfChange: PropTypes.func,
-  };
-
-  render () {
-    const { title, books, loaded, shelfChange } = this.props;
-
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {!loaded && (
-              <p>Loading...</p>
-            )}
-            {loaded && (!books || (books && books.length <= 0)) && (
-              <p>No books to show</p>
-            )}
-            {loaded && books && books.length > 0 && books.map(( book ) => {
-              return (
-                <li key={book.id}>
-                  <Book book={ book } shelfChange={shelfChange} />
-                </li>
-              )
-            })}
-          </ol>
-        </div>
+const BookShelf = ({title, books, loaded, shelfChange}) => {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {!loaded && (
+            <p>Loading...</p>
+          )}
+          {loaded && (!books || (books && books.length <= 0)) && (
+            <p>No books to show</p>
+          )}
+          {loaded && books && books.length > 0 && books.map(( book ) => {
+            return (
+              <li key={book.id}>
+                <Book book={ book } shelfChange={shelfChange} />
+              </li>
+            )
+          })}
+        </ol>
       </div>
-    )
-  };
+    </div>
+  )
+}
+
+BookShelf.propTypes = {
+  title: PropTypes.string.isRequired,
+  books: PropTypes.array,
+  shelfChange: PropTypes.func,
 }
 
 export default BookShelf;
