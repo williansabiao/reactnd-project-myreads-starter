08bacc67a9ddad1fb17e377cf2b33174a793ba7c
Willian R. Sabião <willianjedai@gmail.com>
2017-10-25 13:27:50 +0100

    refactor: Routes component to Stateless Functional Component

--- src/Routes.js
+++ src/Routes.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
 import MyReads from './pages/MyReads';
 import Search from './pages/Search';
 import PropTypes from 'prop-types';
 
-class Routes extends Component {
-  static propTypes = {
-    booksOnShelf: PropTypes.array.isRequired,
-    updateBooksOnShelf: PropTypes.func.isRequired,
-  };
-
-  render () {
-    let { updateBooksOnShelf, booksOnShelf } = this.props;
-    return (
-      <div>
-        <Route exact path='/' render={() => <MyReads updateBooksOnShelf={updateBooksOnShelf} /> } />
-        <Route exact path='/search' render={(route) => <Search route={route} updateBooksOnShelf={updateBooksOnShelf} booksOnShelf={booksOnShelf} />} />
-      </div>
-    )
-  };
+const Routes = ({updateBooksOnShelf, booksOnShelf}) => {
+  return (
+    <div>
+      <Route exact path='/' render={() => <MyReads updateBooksOnShelf={updateBooksOnShelf} /> } />
+      <Route exact path='/search' render={(route) => <Search route={route} updateBooksOnShelf={updateBooksOnShelf} booksOnShelf={booksOnShelf} />} />
+    </div>
+  )
 }
+
+Routes.propTypes = {
+  booksOnShelf: PropTypes.array.isRequired,
+  updateBooksOnShelf: PropTypes.func.isRequired,
+}
+
 
 export default Routes;
